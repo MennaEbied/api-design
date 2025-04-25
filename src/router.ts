86aa6b4064b6ab5a7b299475ad5ab1e66a66d3a1
MennaEbied <mennahesham253@gmail.com>
2025-04-25 20:56:42 +0300

    Handle updates

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,8 @@
 import {Router} from 'express';
 import { body, oneOf, validationResult } from 'express-validator';
 import { handleInputErrors } from './modules/middleware';
-import { createProduct, getOneProduct, getProducts } from './handlers/product';
+import { createProduct, deleteProduct, getOneProduct, getProducts } from './handlers/product';
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update';
 
 
 const router = Router()
@@ -10,29 +11,32 @@ const router = Router()
  * Products
  */
 router.get('/product', getProducts )
-router.get('/product/:id', () => {})
+router.get('/product/:id', getOneProduct)
 router.put('/product/:id', body('name').isString(), handleInputErrors ,(req, res) => {
    
 })
 router.post('/product', body('name').isString(), handleInputErrors ,createProduct)
-router.delete('/product/:id', () => {})
+router.delete('/product/:id',deleteProduct)
 
 /**
  * Updates
  */
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
-router.put('/update/:id',
-    body("title").optional(),
-    body("body").optional(),
-    body("status").isIn(['IN_RPGRESS','SHIPPED','DEPRECATED']),
-    body("version").optional(), 
-    () => {})
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
+router.put('/update/:id', 
+  body('title').optional(),
+  body('body').optional(),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+  body('version').optional(),
+  updateUpdate
+)
 router.post('/update',
-    body("title").exists().isString(),
-    body("body").exists().isString(),
-    () => {})
-router.delete('/update/:id', () => {})
+  body('title').exists().isString(),
+  body('body').exists().isString(),
+  body('productId').exists().isString(),
+  createUpdate
+)
+router.delete('/update/:id', deleteUpdate)
 
 /**
  * Update point
