7b56ebce56992b51b96d00b99abf4cdc0edc3eb3
MennaEbied <mennahesham253@gmail.com>
2025-04-28 20:40:30 +0300

    Error handler

--- src/server.ts
+++ src/server.ts
@@ -10,15 +10,18 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extented:true}))
 
-app.get('/', (req, res) => {
-    console.log('hello from express')
-    res.status(200)
-    res.json({message: "hello"})
+app.get('/', (req, res, next) => {
+   res.json({message:'hello'})
 })
 
 app.use('/api', protect, router)
 
 app.post('/user', createNewUser)
 app.post('/signin', signin)
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.json({message: `had an error: ${err.message}`})
+  })
 
 export default app
